e7495f3853c5d25a36d5f6e57894cd0dee6a4f50
Lucas <lucasferrazl@hotmail.com>
2023-02-08 18:59:03 -0300

    Add functionality to "paint" grid elements

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gridSpace = document.querySelector(".gridspace");
 
+
+// Function for creating a grid of (n x n) --> n is number assigned by user
 function createGrid(number) {
 
     let child = gridSpace.firstElementChild;
@@ -14,12 +16,50 @@ function createGrid(number) {
     for (i=0; i<number**2; i++) {
         let grid = document.createElement("div");
         grid.classList.add("grid");
-        grid.style.width = gridSize + "px"
-        grid.style.height = gridSize + "px"
+        grid.style.width = gridSize + "px";
+        grid.style.height = gridSize + "px";
         gridSpace.appendChild(grid);
     }
 
-   // const grids = document.getElementsByClassName("grid")
-    //grids.style.width = gridSize + "px"
-    //grids.style.height = gridSize + "px"
 }
+
+// Initial grid is 16x16
+createGrid(16);
+
+let setColor = "black"
+
+// ---------------------------------------------------------------------
+// Event listeners to paint each element of the grid when clicked, or moved over while clicked
+
+let clicked = false;
+
+window.addEventListener("mousedown", e => {
+    clicked = true;
+})
+
+window.addEventListener("mouseup", e => {
+    clicked = false;
+})
+
+// Event listener for mousemove - only works when isClicked == true
+gridSpace.addEventListener("mousemove", function(e) {
+    const target = e.target;
+
+    if (clicked == true) {
+        if (target.matches(".grid")) {
+            target.style.backgroundColor = setColor;
+        }
+    }
+
+});
+
+// Listener for click event
+gridSpace.addEventListener("click", function(e) {
+    const target = e.target;
+
+    if (target.matches(".grid")) {
+        target.style.backgroundColor = setColor;
+    }
+});
+
+// -------------------------------------------------------------------------
