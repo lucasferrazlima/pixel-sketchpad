f0898232890c2ff06e034ce79107156cda7e72d4
Lucas <lucasferrazl@hotmail.com>
2023-02-09 15:22:31 -0300

    Add css style elements

--- script.js
+++ script.js
@@ -100,3 +100,20 @@ gridButton.addEventListener("click", function() {
         gridVisible = true;
     }
 })
+
+Coloris({
+    theme: "polaroid",
+    swatches: [
+        '#264653',
+        '#2a9d8f',
+        '#e9c46a',
+        'rgb(244,162,97)',
+        '#e76f51',
+        '#d62828',
+        'navy',
+        '#07b',
+        '#0096c7',
+        '#00b4d880',
+        'rgba(0,119,182,0.8)'
+      ]
+});
